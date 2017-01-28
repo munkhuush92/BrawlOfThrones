2cd97afb0df90bab5f806f1d2a4594a5f731b96d
tbs95 <tbs95@uw.edu>
2017-01-27 23:02:06 -0800

    Update main.js
    Added my character's code

--- main.js
+++ main.js
@@ -153,11 +153,79 @@ Unicorn.prototype.draw = function (ctx) {
     Entity.prototype.draw.call(this);
 }
 
+/*
+Code added by Travis (Tbs95). This will add my character but both characters cannot punch at the same time for some reason.
+*/
+function ChunLi(game) {
+  // function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
+  this.animation = new Animation(ASSET_MANAGER.getAsset("./img/chunli.png"),          0, 0,   150, 150, 0.1, 7, true, false);
+  this.leftWalkAnimation = new Animation(ASSET_MANAGER.getAsset("./img/chunli.png"),  0, 150, 150, 150, 0.1, 18, true, false);
+  this.rightWalkAnimation = new Animation(ASSET_MANAGER.getAsset("./img/chunli.png"), 0, 300, 150, 150, 0.1, 16, true, false);
+  this.boomAnimation = new Animation(ASSET_MANAGER.getAsset("./img/chunli.png"),      0, 750, 150, 150, 0.1, 6, false, false);
+  //testing
+  this.booming = false;
+  //endoftesting
+  this.radius = 100;
+  this.speed  = 100;
+  this.ground = 400;
+  Entity.call(this, game, 50, 100);
+}
+
+ChunLi.prototype = new Entity();
+ChunLi.prototype.constructor = ChunLi;
+
+ChunLi.prototype.update = function () {
+    //test
+    if(this.game.boom){
+      this.booming = true;
+    }
+    if(this.booming){
+      if(this.boomAnimation.isDone()){
+        this.boomAnimation.elapsedTime = 0;
+        this.booming = false;
+      }
+
+    }
+    //endoftesting
+  
+    if (this.x > 800) this.x = -230;
+    if(Key.isDown(Key.RIGHT)){
+      this.x += this.game.clockTick * this.speed;
+    }else if (Key.isDown(Key.LEFT)){
+      this.x -= this.game.clockTick * this.speed;
+
+    } if(Key.isDown(Key.UP)){
+      this.y -= this.game.clockTick * this.speed;
+    }
+    else if(Key.isDown(Key.DOWN)){
+      this.y += this.game.clockTick * this.speed;
+    }
+    Entity.prototype.update.call(this);
+}
+
+ChunLi.prototype.draw = function (ctx) {
+  if(this.booming){
+      this.boomAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    }else {
+    
+     if(Key.isDown(Key.RIGHT)){
+      this.rightWalkAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    }else if (Key.isDown(Key.LEFT)){
+      this.leftWalkAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+
+    }else{
+      this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    }
+    }
+    Entity.prototype.draw.call(this);
+}
+
 // the "main" code begins here
 
 var ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("./img/kenbig.png");
+ASSET_MANAGER.queueDownload("./img/chunli.png");
 ASSET_MANAGER.queueDownload("./img/background.jpg");
 ASSET_MANAGER.downloadAll(function () {
     console.log("starting up da sheild");
@@ -166,12 +234,14 @@ ASSET_MANAGER.downloadAll(function () {
 
     var gameEngine = new GameEngine();
     var unicorn = new Unicorn(gameEngine);
+    var chun_li = new ChunLi(gameEngine);  
 	
 	
     gameEngine.init(ctx);
     gameEngine.start();
-	var bg = new Background(gameEngine, ASSET_MANAGER.getAsset("./img/background.jpg"));
-	gameEngine.addEntity(bg);
+    var bg = new Background(gameEngine, ASSET_MANAGER.getAsset("./img/background.jpg"));
+    gameEngine.addEntity(bg);
     gameEngine.addEntity(unicorn);
+    gameEngine.addEntity(chun_li);  
 	
 });
